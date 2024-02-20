27b203292d9cfe779cfec1c56bb44ce9da0a6bc7
Mindaugas Minelga <minelga.m@gmail.com>
2024-02-20 21:03:08 +0200

    modify main.py file

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-hello
+import datetime
+from sqlalchemy import Column, Integer, String, Float, DateTime, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///shop_items.db")
+Base = declarative_base()
+
+
+class Item(Base):
+
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True)
+    name = Column("name", String)
+    # amount = Column("amount", Integer)
+    # price = Column("price", Float)
+    # best_before = Column("best before", DateTime)
+    # day_entered = Column("day of creation", DateTime)
+    # item_type = Column("type of item", String)
+
+
+class ManageItems:
+
+    def create_item(self) -> None:
+        pass
+
+    def name_to_capitals(self) -> str:
+        return self.name.upper()
+
+
+item_one = Item(name="Rope")
+
+name = ManageItems.name_to_capitals(item_one)
+
+print(name)
---
+++ uzduotis.py
@@ -0,0 +1,12 @@
+# Create a shop item  manage system. The program main aim is to take inputs (create) from user (item name, amount, price, best_before if aplicable, date entered, item_type (electronic etc)):
+#  - The name should be converted all capitals
+#  - amount can't be negative
+#  - price should be float.
+#  - best bofore should be a date (YYYY-MM-DD)
+#  - date entered should be (YYYY-MM-DD hh:mm:ss)
+#  - total price value of entered items
+
+# All methods in a class should have a error handling and logging handling through decorators.
+# All data must be saved and retrieved from sql database.
+# After submiting data I should be able to retreive all data from database based on item_type.
+# error handlinimas tik su decoratoriais
